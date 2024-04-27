86366a6c5ef3d2a829e058b93e7a631bda278fd6
namigismylv <tu7ew05s4@code.edu.az>
2024-04-27 14:43:59 +0400

    last changes

--- index.js
+++ index.js
@@ -1,51 +1,71 @@
-fetch("http://www.omdbapi.com/?i=tt3896198&apikey=7d106634")
-.then((response)=>response.json())
-.then((data)=>{
-    const poster=document.getElementById("poster")
-    function renderUI(cards){
-         poster.innerHtml=""
-             poster.innerHtml +=`
-             <div class="poster__top">
-                     <input type="text">
-                     <button>Search</button>
-                 </div>
-                 <div class="poster__middle">
-                     <div class="poster__middle__left">
-                         <img src="https://m.media-amazon.com/images/I/71M8YFEakfL._AC_UF894,1000_QL80_.jpg" alt="">
-                     </div>
-                     <div class="poster__middle__right">
-                         <h1>${cards.Title}</h1>
-                         <div class="rating">
-                             <i class="fa-solid fa-star"></i>
-                             <p>8.1</p>
-                         </div>
-                         <div class="poster__middle__right__m">
-                             <p>TV-14</p>
-                             <p>2022-</p>
-                             <p>N/A</p>
-                         </div>
-                         <div class="genree">
-                             <p class="">Comedy</p>
-                             <p>Crime</p>
-                             <p>Fantasy</p>
-                         </div>
-                     </div>
-         
-                 </div>
-                 <div class="poster__body">
-                     <div class="plot">
-                         <h3>Plot:</h3>
-                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, placeat.</p>
-                     </div>
-                     <div class="actors">
-                         <h3>Cast:</h3>
-                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, placeat.</p>
-                     </div>
-                 </div>
-             
-             `
-            
-         }
+document.addEventListener("DOMContentLoaded", () => {
+    const input = document.getElementById("input");
+    const searchBtn = document.getElementById("searchBtn");
+    const posterBottom = document.getElementById("poster__bottom");
 
-         renderUI(data)
-    })
+    searchBtn.addEventListener("click", () => {
+        const searchTerm = input.value;
+        searchFilm(searchTerm);
+        input.value=""
+    });
+
+    function searchFilm(searchTerm) {
+        fetch(`http://www.omdbapi.com/?t=${searchTerm}&apikey=7d106634`)
+            .then((response) => response.json())
+            .then((data) => {
+                renderUI(data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                posterBottom.innerHTML = `<p id="movie">Movie not found...</p>`;
+            });
+    }
+
+    const renderGenres = (genres) => {
+        let genreHTML = '';
+        const genreList = genres.split(',');
+        for (let i = 0; i < genreList.length; i++) {
+            const genre = genreList[i].trim();
+            genreHTML += `<p>${genre}</p>`;
+        }
+        return genreHTML;
+    };
+
+
+    function renderUI(cards) {
+        const { Title, imdbRating, Rated, Year, Website, Genre, Plot, Actors, Poster } = cards;
+        posterBottom.innerHTML = "";
+        posterBottom.innerHTML += `
+            <div class="poster__middle">
+                
+                    <img  src="${Poster}" alt="" width=180px>
+                
+                <div class="poster__middle__right">
+                    <h1>${Title}</h1>
+                    <div class="rating">
+                        <i class="fa-solid fa-star"></i>
+                        <p>${imdbRating}</p>
+                    </div>
+                    <div class="poster__middle__right__m">
+                        <p>${Rated}</p>
+                        <p>${Year}</p>
+                        <p>${Website}</p>
+                    </div>
+                    <div class="genree">
+                        ${renderGenres(Genre)}
+                    </div>
+                </div>
+            </div>
+            <div class="poster__body">
+                <div class="plot">
+                    <h3>Plot:</h3>
+                    <p>${Plot}</p>
+                </div>
+                <div class="actors">
+                    <h3>Cast:</h3>
+                    <p>${Actors}</p>
+                </div>
+            </div>
+        `;
+    }
+});
